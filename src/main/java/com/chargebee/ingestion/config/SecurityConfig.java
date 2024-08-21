6f5d7924d198d58bc178cfde0515f054a3711ce5
Kiruba Ebenezer <40791548+cb-kirubaebenezer@users.noreply.github.com>
2024-08-21 17:15:49 +0530

    fix: added logger config

--- src/main/java/com/chargebee/ingestion/config/SecurityConfig.java
+++ src/main/java/com/chargebee/ingestion/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.chargebee.ingestion.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -41,11 +43,14 @@ public class SecurityConfig {
         private static final String API_KEY_HEADER = "X-API-Key";
         private static final String VALID_API_KEY = "KB73Xz7kH7F14vcSZLSoEXcJaL1VnC3xWR67MOtBBMxkkBnkkYoxDSqcKtYTzxB33zHE0p5IZsSsVFJczYxUt0atgTvhAYVyMPZd34JCgeUDj1gQywtxMkC1QXabLGKl";
 
+        private final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
+
+
         @Override
         protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
                 throws ServletException, IOException {
             String apiKey = request.getHeader(API_KEY_HEADER);
-
+            logger.info("Request URI {}, API Key {}", request.getRequestURI(), apiKey);
             if (VALID_API_KEY.equals(apiKey)
                     || request.getRequestURI().equals("/actuator/health")
                     ||  request.getRequestURI().equals("/actuator/info")) {
--- src/main/java/com/chargebee/ingestion/controllers/KafkaController.java
+++ src/main/java/com/chargebee/ingestion/controllers/KafkaController.java
@@ -4,9 +4,14 @@ import com.chargebee.ingestion.service.KafkaTopicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping("/api/kafka")
 public class KafkaController {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaController.class);
 
     @Autowired
     private KafkaTopicService kafkaTopicService;
@@ -14,9 +19,11 @@ public class KafkaController {
     @PostMapping("/create-topic")
     public String createTopic(@RequestParam String topicName, @RequestParam int partitions, @RequestParam short replicationFactor) {
         try {
+            logger.info("Received request to create topic: {}, partitions: {}, replicationFactor: {}", topicName, partitions, replicationFactor);
             kafkaTopicService.createTopic(topicName, partitions, replicationFactor);
             return "Topic created successfully";
         } catch (Exception e) {
+            logger.info("Topic {} created successfully", topicName);
             return "Error creating topic: " + e.getMessage();
         }
     }
