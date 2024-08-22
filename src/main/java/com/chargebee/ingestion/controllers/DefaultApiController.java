9bac009b1b23efa15638a30cf404c134862cce7f
Kiruba Ebenezer <40791548+cb-kirubaebenezer@users.noreply.github.com>
2024-08-22 19:13:41 +0530

    Update DefaultApiController.java

--- src/main/java/com/chargebee/ingestion/controllers/DefaultApiController.java
+++ src/main/java/com/chargebee/ingestion/controllers/DefaultApiController.java
@@ -25,7 +25,7 @@ public class DefaultApiController implements DefaultApi {
 
     @Override
     public ResponseEntity<GetInfo200Response> getInfo() {
-        return new ResponseEntity<>(new GetInfo200Response().status("200").description("UPV1"), HttpStatus.OK);
+        return new ResponseEntity<>(new GetInfo200Response().status("200").description("UPV2"), HttpStatus.OK);
     }
 
     @Override
